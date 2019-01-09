b37ec7096453e8a3e597db2f598a9d74fead3e05
Shuma Yoshioka <s64.stdio@gmail.com>
2019-01-09 18:35:42 +0900

    Revert ":bug:"
    This reverts commit f8fae4797f0c7f3b5b90e95b1362c1edcc190114.

--- app/src/main/java/jp/s64/android/example/MainActivity.kt
+++ app/src/main/java/jp/s64/android/example/MainActivity.kt
@@ -5,8 +5,6 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
-    private val unusedProperty: String = ""
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
